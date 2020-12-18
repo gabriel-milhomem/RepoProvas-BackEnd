253177421b03e3f72943905529eedb9b05c37e23
Gabriel Milhomem <gabriell.mil@gmail.com>
2020-12-18 16:55:51 -0300

    deploy

--- src/database/index.js
+++ src/database/index.js
@@ -2,11 +2,7 @@ require("dotenv").config();
 const { Pool } = require('pg');
 
 const connection = new Pool({ 
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE
+    connectionString: process.env.DATABASE_URL
 });
 
 module.exports = connection;
